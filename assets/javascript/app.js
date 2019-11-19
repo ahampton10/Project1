16a6c00790eef6ebaf0e27f9591180d6c3b1c266
Blake Tompkins <blaketompkins99@gmail.com>
2019-11-18 20:50:33 -0600

    Added css to the tickets page

---
+++ assets/javascript/app.js
@@ -0,0 +1,6 @@
+// 1. Creating on click functions for each of the teams to pull up stats
+// 2. Purchasing tickets
+    // Storing user information if firebase
+        // Name
+        // Card
+        // # of tickets purchased
